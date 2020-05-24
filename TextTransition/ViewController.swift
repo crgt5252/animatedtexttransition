2b07d664eb786af3cc4ea7e1e77894d739b0cb8a
Christopher Taylor <crgt5252@gmail.com>
2020-05-24 11:04:26 -0700

    Initial Commit

---
+++ TextTransition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextTransition
+//
+//  Created by Christopher Taylor on 5/24/20.
+//  Copyright © 2020 Christopher Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TextTransitionTests/TextTransitionTests.swift
@@ -0,0 +1,34 @@
+//
+//  TextTransitionTests.swift
+//  TextTransitionTests
+//
+//  Created by Christopher Taylor on 5/24/20.
+//  Copyright © 2020 Christopher Taylor. All rights reserved.
+//
+
+import XCTest
+@testable import TextTransition
+
+class TextTransitionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
